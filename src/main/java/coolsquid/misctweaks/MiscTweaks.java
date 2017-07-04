a136237d867d63c6e92d0831593dd0f4f6245476
CoolSquid <mail@coolsquid.me>
2017-07-04 18:19:33 +0200

    Change version number to 1.1.1

--- src/main/java/coolsquid/misctweaks/MiscTweaks.java
+++ src/main/java/coolsquid/misctweaks/MiscTweaks.java
@@ -19,7 +19,7 @@ public class MiscTweaks {
 
 	public static final String MODID = "misctweaks";
 	public static final String NAME = "MiscTweaks";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	public static final String DEPENDENCIES = "required-after:forge@[14.21.1.2387,);after:AppleCore";
 	public static final String UPDATE_JSON = "https://coolsquid.me/api/version/misctweaks.json";
 
